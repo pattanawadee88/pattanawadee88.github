c40b5cc728de7b82e5cf8d658f970b22891fc4e0
pattanawadee88 <winyaratp@wit.edu>
2021-07-01 10:55:12 -0400

    added style to project-content:hover

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.querySelector("#home-section").style.display = "block"
     document.querySelector("#project-section").style.display = "block"
+    document.querySelector(".form-container").style.display = "block"
+    document.querySelector("footer").style.display = "block"
 }
 
 window.onscroll = function() {scrollFunction()};
@@ -16,8 +18,9 @@ function scrollFunction() {
   if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
     document.getElementById("project-header").style.opacity = "1";
     document.getElementById("project-header").style.transition = "opacity 0.5s";
-    document.getElementById("project-header").style.transitionTimingFunction = "ease-in";
-  } else {
+    document.getElementById("project-header").style.transitionTimingFunction = "ease-in"; 
+  } else{
     document.getElementById("project-header").style.opacity = "0.1";
+  
   }
 }
