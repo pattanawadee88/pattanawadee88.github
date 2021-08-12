2665ef1adaa2363c2b16b58182a7c26eb83416a0
pattanawadee88 <winyaratp@wit.edu>
2021-08-12 14:50:39 -0400

    delete all code in App.js

--- App.js
+++ App.js
@@ -1,15 +1,1 @@
-let loadingTime;
-
-function pageLoading() {
-    loadingTime = setTimeout(showPage, 1000);
-}
-        
-function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.querySelector("#home-section").style.display = "block";
-    document.querySelector("#project-section").style.display = "block"
-    document.querySelector(".form-container").style.display = "block"
-    document.querySelector("footer").style.display = "block"
-}
-
 
