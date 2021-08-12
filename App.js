c1d0069f08f0474e7bb3957dbbba9b422602c8aa
pattanawadee88 <winyaratp@wit.edu>
2021-08-12 16:30:18 -0400

    update font size

--- App.js
+++ App.js
@@ -1,1 +1,6 @@
+document.getElementById("ham-nav").addEventListener('click', ()=>{
+    document.getElementById("main-nav").style.display = 'block';
+    document.querySelector("ham-nav").style.display = 'none';
 
+    
+})
