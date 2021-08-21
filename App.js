07aaca19607be94f8ee5809d27fe8de5dc401203
pattanawadee88 <winyaratp@wit.edu>
2021-08-21 14:06:07 -0400

    update style

--- App.js
+++ App.js
@@ -1,18 +1,73 @@
+// const scrollOffset = 120;
+ 
+// const scrollElement = document.querySelector(".scroll");
+ 
+// const elementInView = (el, offset = 0) => {
+//   const elementTop = el.getBoundingClientRect().top;
+ 
+//   return (
+//     elementTop <= 
+//     ((window.innerHeight || document.documentElement.clientHeight) - offset)
+//   );
+// };
+ 
+// const displayScrollElement = () => {
+//   scrollElement.classList.add('scrolled');
+// }
+ 
+// const hideScrollElement = () => {
+//   scrollElement.classList.remove('scrolled');
+// }
+ 
+// const handleScrollAnimation = () => {
+//   if (elementInView(scrollElement, scrollOffset)) {
+//       displayScrollElement();
+//   } else {
+//     hideScrollElement();
+//   }
+// }
+ 
+// window.addEventListener('scroll', () => {
+//   handleScrollAnimation();
+// })
 
+const scrollElements = document.querySelectorAll(".scroll");
 
-function doSomething() {
-    console.info('DOM loaded');
-  }
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
 
-  if (document.readyState === 'loading') { 
-    console.info('hi'); // Loading hasn't finished yet
-    // document.addEventListener('DOMContentLoaded', () =>{
-    //     console.info('hi');
-        
-    // } );
-  } else {  // `DOMContentLoaded` has already fired
-    doSomething();
-  }
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
 
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
 
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
 
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
