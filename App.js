b5a1aafc391a44f9eb8c4d2e1d448f2b88009cf4
pattanawadee88 <winyaratp@wit.edu>
2021-08-22 15:47:12 -0400

    add scroll animation

--- App.js
+++ App.js
@@ -1,41 +1,5 @@
-// const scrollOffset = 90;
- 
-// const scrollElement = document.querySelector(".scroll");
- 
-// const elementInView = (el, offset = 0) => {
-//   const elementTop = el.getBoundingClientRect().top;
- 
-//   return (
-//     elementTop <= 
-//     ((window.innerHeight || document.documentElement.clientHeight) - offset)
-//   );
-// };
- 
-// const displayScrollElement = () => {
-//   scrollElement.classList.add('scrolled');
-// }
- 
-// const hideScrollElement = () => {
-//   scrollElement.classList.remove('scrolled');
-// }
- 
-// const handleScrollAnimation = () => {
-//   if (elementInView(scrollElement, scrollOffset)) {
-//       displayScrollElement();
-//   } else {
-//     hideScrollElement();
-//   }
-// }
- 
-// window.addEventListener('scroll', () => {
-//   handleScrollAnimation();
-// })
 
 const scrollElements = document.querySelectorAll(".scroll");
-const pro = document.querySelector("#projects-container")
-const length = pro.getBoundingClientRect().top;
-console.log(window.innerHeight)
-console.log(length)
 const scrollOffset = 300;
 const elementInView = (el, offset) => {
   const elementTop = el.getBoundingClientRect().top;
